11863d88e12a65a5c343a738ffac269e2f6e1cf7
vrastil <vrastil@fzu.cz>
2019-05-17 16:05:58 +0200

    updat pull, from anywhere

--- shub_pull.sh
+++ shub_pull.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+cd /home/users/vrastil/GIT/FastSim/install
 singularity pull shub://vrastil/FastSim-Container
+cd ../jobs
+ln -sf ../install/vrastil-FastSim-Container-master-latest.simg fastsim.img
+
