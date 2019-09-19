15ebcf7951a8c2feb8e2f0ffd7afa10cbe9de59f
vrastil <vrastil@fzu.cz>
2019-09-19 15:56:41 +0200

    upd to Singularity 3

--- shub_pull.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd /home/users/vrastil/GIT/FastSim/install
-singularity pull shub://vrastil/FastSim-Container
-cd ../jobs
-ln -sf ../install/vrastil-FastSim-Container-master-latest.simg fastsim.img
-
